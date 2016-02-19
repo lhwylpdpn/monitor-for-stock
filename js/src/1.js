4c4a6d79d5b6900c17af51b4886c475a99a8d0bc
liuhao <58254451@qq.com>
2016-02-19 18:07:07 +0800

    合并同一台机器看两个平台版本

---
+++ js/src/1.js
@@ -0,0 +1,74 @@
+$(function () {
+    var seriesOptions = [],
+        seriesCounter = 0,
+        names = ['tracking_Balance', 'tracking_Equity','tracking_AccountMargin'],
+        // create the chart when all data is loaded
+        createChart = function () {
+
+            $('#container').highcharts('StockChart', {
+
+            rangeSelector : {
+                buttons : [{
+                    type : 'hour',
+                    count : 1,
+                    text : '1h'
+                }, {
+                    type : 'day',
+                    count : 1,
+                    text : '1D'
+                }, {
+                    type : 'all',
+                    count : 1,
+                    text : 'All'
+                }],
+                selected :1,
+                inputEnabled : true
+            },
+
+                yAxis: {
+                    labels: {
+                        formatter: function () {
+                            return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                        }
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 2,
+                        color: 'silver'
+                    }]
+                },
+
+                plotOptions: {
+                    series: {
+                        compare: 'value'
+                    }
+                },
+
+                tooltip: {
+                    pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                    valueDecimals: 2
+                },
+
+                series: seriesOptions
+            });
+        };
+
+    $.each(names, function (i, name) {
+
+        $.getJSON('EB3C3B239AFB8B62B7EC3451D269EB1E/MQL4/Files/json/' + name.toLowerCase() + '.json',    function (data) {
+
+            seriesOptions[i] = {
+                name: name,
+                data: data
+            };
+
+            // As we're loading the data asynchronously, we don't know what order it will arrive. So
+            // we keep a counter and create the chart when all the data is loaded.
+            seriesCounter += 1;
+
+            if (seriesCounter === names.length) {
+                createChart();
+            }
+        });
+    });
+});
---
+++ js/src/load_json_1.js
@@ -0,0 +1,42 @@
+$(function() {
+	var seriesOptions = [],
+		yAxisOptions = [],
+		seriesCounter = 0,
+		names = ['static'];
+	
+	var path = "EB3C3B239AFB8B62B7EC3451D269EB1E/MQL4/Files/json/";
+	$.each(names, function(i, name) {
+	
+		$.getJSON(path+ name.toLowerCase() +'.json',	function(result) {
+			
+			var data = result;
+
+	
+
+			// As we're loading the data asynchronously, we don't know what order it will arrive. So
+			// we keep a counter and create the chart when all the data is loaded.
+			seriesCounter++;
+
+			if (seriesCounter == names.length) {
+			
+				//document.getElementById("data1").innerHTML='<h1>'+result.avg_profit*100+'%</h1>'
+				//document.getElementById("data2").innerHTML='<h1>'+result.avg_profit_week*100+'%</h1>'
+				document.getElementById("data3").innerHTML='<div class="progress"><div class="progress-bar progress-bar-success" style="width:'+result.success*100+'%"><span >'+result.success*100+'%</span></div><div class="progress-bar progress-bar-warning progress-bar-striped" style="width: '+(1-result.success)*100+'%"><span >'+(1-result.success)*100+'%</span></div></div><div class="progress"><div class="progress-bar progress-bar-success" style="width:'+result.success_profit*100+'%"><span>'+result.success_profit*100+'%</span></div><div class="progress-bar progress-bar-warning progress-bar-striped" style="width:'+(1-result.success_profit)*100+'%"><span >'+(1-result.success_profit)*100+'%</span></div></div>'
+				document.getElementById("data4").innerHTML='<h1>'+result.jiasheng_status+'</h1>'
+				document.getElementById("data5").innerHTML='<h1>'+result.lmx_status+'</h1>'
+				document.getElementById("data6").innerHTML='<h1>'+result.avg_num+'</h1>'
+				document.getElementById("data7").innerHTML=result.table1
+
+//"                <a href="#" class="list-group-item list-group-item-success">日常生活支出<span class="badge">11500</span><span class="badge">11500</span></a>
+ //               <a href="#" class="list-group-item list-group-item-info">电费余额  <span class="badge">532.5</span></a>
+    //            <a href="#" class="list-group-item list-group-item-warning">水费和煤气费余额   <span class="badge">99</span></a>
+  //              <a href="#" class="list-group-item list-group-item-danger">固定购物账户余额  <span class="badge">1934.5</span></a>
+      //          <a href="#" class="list-group-item list-group-item-dark">物业费待缴纳日期  <span class="badge">2016-1-3</span></a>"
+
+
+
+			}
+		});
+	});
+
+});	
